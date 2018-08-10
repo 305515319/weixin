d6487a347f59088a97dc51151747be7655366f79
1 <1>
2018-08-10 18:09:32 +0800

    1

--- application/user/controller/Register.php
+++ application/user/controller/Register.php
@@ -4,13 +4,28 @@ use think\Controller;
 use think\Db;
 class Register extends Controller
 {
-	public function index()
+    public function __construct()
+    {
+        parent::__construct();
+        $user_auth      = session('user_auth');
+        $user_auth_sign = session('user_auth_sign');
+        $isLogin = $user_auth_sign == data_signature($user_auth) ? $user_auth['uid'] : 0;
+        $this->assign('islogin',$isLogin);
+    }
+
+    public function index()
 	{
 		return view();
 	}
+	public function forgot()
+    {
+        return view();
+    }
+
 	public function doReg()
     {
         $username       = input('username');
+        $email          = input('email');
         $password       = input('password');
         $repassword     = input('repassword');
         $noncestr       = random(6);
@@ -18,18 +33,48 @@ class Register extends Controller
         if(!$username || !$password || !$repassword)                      return ['errcode'=>0,'errmsg'=>'缺少必要参数'];
         if($repassword != $password)                                      return ['errcode'=>0,'errmsg'=>'两次密码输入不一致'];
         if(Db::name('user')->where('user_name',$username)->value('user_id')) return ['errcode'=>0,'errmsg'=>'用户已存在'];
+        if(Db::name('user')->where('user_email',$email)->value('user_id')) return ['errcode'=>0,'errmsg'=>'Email已被注册'];
 
         $data = [
             'user_name' =>$username,
+            'user_email' =>$email,
             'user_pass' =>encrypt_password($password,$noncestr),
             'noncestr'  =>$noncestr,
             'create_time' =>date('Y-m-d H:i:s')
         ];
         $uid = Db::name('user')->insertGetId($data);
         if($uid){
+            $userData = ['uid'=>$uid,'user_name'=>$username];
+            session('user_auth',$userData);
+            session('user_auth_sign',data_signature($userData));
             return ['errcode'=>1,'errmsg'=>'注册用户成功'];
         } else {
             return ['errcode'=>0,'errmsg'=>'写入数据库失败'];
         }
     }
+
+    public function doForgot()
+    {
+        $username = input('username');
+        $email = input('email');
+        if(!request()->isAjax())                                          return ['errcode'=>0,'errmsg'=>'请求类型错误'];
+        if(!$username || !$email)                                         return ['errcode'=>0,'errmsg'=>'缺少必要参数'];
+        $user = Db::name('user')->where('user_name',$username)->field('user_id,user_email')->find();
+        if(!$user['user_id'])                                             return ['errcode'=>0,'errmsg'=>'用户不存在'];
+        if(!$user['user_email'])                                          return ['errcode'=>0,'errmsg'=>'该账号未设置Email，请联系客服 q'];
+        if($email != $user['user_email'])                                 return ['errcode'=>0,'errmsg'=>'Email地址不匹配'];
+        $noncestr = random(6);
+        Db::name('user')->where('user_name',$username)->update([
+            'user_pass' =>encrypt_password($noncestr,$noncestr),
+            'noncestr'  =>$noncestr,
+        ]);
+        $content = '您好，这是来自wx1234.cn团队的一封邮件，您的账号：'.$username.'。新密码为：'.$noncestr.'。请登录 <a href="https://www.wx1234.cn">https://www.wx1234.cn</a> 站点及时修改您的密码！';
+        $res = sendMail($email,$content);
+        if($res){
+            return ['errcode'=>1,'errmsg'=>'新密码已发送至您的邮箱，请注意查收'];
+        }else {
+            return ['errcode'=>0,'errmsg'=>'发送邮件失败，请联系客服'];
+        }
+    }
+
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,31 +6,79 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit34a41e2841af1a67f3ddef099fc7b348
 {
+    public static $files = array (
+        '9b552a3cc426e3287cc811caefa3cf53' => __DIR__ . '/..' . '/topthink/think-helper/src/helper.php',
+        '1cfd2761b63b0a29ed23657ea394cb2d' => __DIR__ . '/..' . '/topthink/think-captcha/src/helper.php',
+        'ddc3cd2a04224f9638c5d0de6a69c7e3' => __DIR__ . '/..' . '/topthink/think-migration/src/config.php',
+        'cc56288302d9df745d97c934d6a6e5f0' => __DIR__ . '/..' . '/topthink/think-queue/src/common.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         't' => 
         array (
+            'think\\mongo\\' => 12,
+            'think\\migration\\' => 16,
+            'think\\helper\\' => 13,
             'think\\composer\\' => 15,
+            'think\\captcha\\' => 14,
             'think\\' => 6,
         ),
         'a' => 
         array (
             'app\\' => 4,
         ),
+        'P' => 
+        array (
+            'Phinx\\' => 6,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'think\\mongo\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/topthink/think-mongo/src',
+        ),
+        'think\\migration\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/topthink/think-migration/src',
+        ),
+        'think\\helper\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/topthink/think-helper/src',
+        ),
         'think\\composer\\' => 
         array (
             0 => __DIR__ . '/..' . '/topthink/think-installer/src',
         ),
+        'think\\captcha\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/topthink/think-captcha/src',
+        ),
         'think\\' => 
         array (
             0 => __DIR__ . '/../..' . '/thinkphp/library/think',
+            1 => __DIR__ . '/..' . '/topthink/think-image/src',
+            2 => __DIR__ . '/..' . '/topthink/think-queue/src',
         ),
         'app\\' => 
         array (
             0 => __DIR__ . '/../..' . '/application',
         ),
+        'Phinx\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/topthink/think-migration/phinx/src/Phinx',
+        ),
+    );
+
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -38,6 +86,7 @@ class ComposerStaticInit34a41e2841af1a67f3ddef099fc7b348
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit34a41e2841af1a67f3ddef099fc7b348::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit34a41e2841af1a67f3ddef099fc7b348::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit34a41e2841af1a67f3ddef099fc7b348::$classMap;
 
         }, null, ClassLoader::class);
     }
